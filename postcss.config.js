25eda1b139431b7e96c9017758d840f3fe526620
Mac32 <alberth.bompart22@gmail.com>
2021-02-05 02:42:03 -0500

    cambio postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {config: './tailwindcss-config.js'},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
 }
