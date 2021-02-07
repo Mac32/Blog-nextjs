5a918aa3234485ca7eba1d71e46572ee503bf9fa
Mac32 <alberth.bompart22@gmail.com>
2021-02-07 04:34:35 -0500

    Correccion de bug en publicaciones (id)

--- src/pages/post/[id].js
+++ src/pages/post/[id].js
@@ -6,28 +6,31 @@ import loadable from '@loadable/component'
 const PostFooter = loadable(() => import('../../components/PostFooter'))
 const H2 = loadable(() => import('../../components/elements/H2'))
 
-const Post = ({res}) => {
+const Post = ({ res }) => {
   const [post, setPost] = React.useState([]);
-  const backgroundImage = {
-    color: 'blue',
-    backgroundImage: 'url(' + res.urlImage + ')',
-  };
+
 
   React.useEffect(() => {
     setPost(res)
-  }, []);
+  }, [res]);
 
   return (
     <div className="container mx-auto md:w-4/5 shadow-md m-6">
-    <div>
-    <div className="h-48 bg-center bg-cover rounded-t-md" style={backgroundImage}>
-    <img className="hidden" src={post.urlImage} alt={post.descriptionImage} />
-    </div>
-    <H2 texto={post.titulo} />
-    <div className="m-6 sm:m-9 text-gray-700" dangerouslySetInnerHTML={{ __html: post.contenido }}></div>
-    </div>
-    <hr />
-    <PostFooter autor={post.autor} />
+{
+    post && post !== undefined ?
+      <>
+      <div>
+        <div className="h-48 bg-center bg-cover rounded-t-md" style={{ backgroundImage: 'url(' + post.urlImage + ')', }}>
+          <img className="hidden" src={post.urlImage} alt={post.descriptionImage} />
+        </div>
+        <H2 texto={post.titulo} />
+        <div className="m-6 sm:m-9 text-gray-700" dangerouslySetInnerHTML={{ __html: post.contenido }}></div>
+      </div>
+      <hr />
+      <PostFooter autor={post.autor} /> 
+      </>
+      : null
+}
     </div>
   );
 };
@@ -42,13 +45,13 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({params}) {
-  const {id} = params
+export async function getStaticProps({ params }) {
+  const { id } = params
   const docRef = db.collection("fl_content").doc(id);
   //Se obtiene el documento
   const res = await docRef.get().then(doc => {
     const datos = doc.data()
-    return({
+    return ({
       urlImage: datos.urlImage,
       descriptionImage: datos.descriptionImage,
       titulo: datos.titulo,
