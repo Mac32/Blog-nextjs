30322a5affa8ae6843a569a8a582a0e808f49329
Mac32 <alberth.bompart22@gmail.com>
2021-02-07 05:02:40 -0500

    Configuracion de ostcss con huma...

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            content: [
+              './components/**/*.js',
+              './pages/**/*.js'
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          }
+        }
+      : {})
+  }
+};
