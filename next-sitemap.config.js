228440bb7e8c54aec739d68d49938e8070275c61
Alberth Bompart <alberth.bompart22@gmail.com>
2022-06-30 15:10:27 -0500

    correccion de sitemap para exportar modulos node

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,5 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
+module.exports = {
   siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true // (optional)
   // ...other options
 }
-
-export default config
