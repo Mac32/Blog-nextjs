28797bbea755b1a30f9663aede337c9b54a67a5a
Alberth Bompart <alberth.bompart22@gmail.com>
2022-08-15 22:43:52 -0500

    agregado de descripcion a la imagen (alt)

--- components/elements/Avatar.js
+++ components/elements/Avatar.js
@@ -4,7 +4,7 @@ export default function Avatar () {
   return (
     <Link href='/About'>
       <a className='flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white' aria-haspopup='true'>
-        <img className='h-12 w-12  rounded-full' src='https://i.ibb.co/0rmH7Kv/Perfil.png' alt='' />
+        <img className='h-12 w-12  rounded-full' src='https://i.ibb.co/0rmH7Kv/Perfil.png' alt='foto de perfil' />
       </a>
     </Link>
   )
